1dabbe664befc2f5b8aa4214f6b09c07494e4329
Talha Imran <talhaimran284@gmail.com>
2020-12-26 20:24:24 +0500

    Used dotenv to hide the connection string and port no

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-let connectionString = `mongodb+srv://talhaimran:talha157@cluster0.u8ext.gcp.mongodb.net/ConnectDev?retryWrites=true&w=majority`;
+dotenv.config();
+// let connectionString = `mongodb+srv://talhaimran:talha157@cluster0.u8ext.gcp.mongodb.net/ConnectDev?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
-    mongoose.connect(connectionString, {
+    mongoose.connect(process.env.CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const connectDB = require('./db');
 const app = express();
 
+dotenv.config();
+
 app.get('/', (req, res) => {
-  res.send('Api working fine!');
+  // res.send('Api working fine!');
+  res.json({ developerName: 'Talha Imran', age: 23 , gender: "Male"});
 });
 
 const PORT = process.env.PORT || 5000;
