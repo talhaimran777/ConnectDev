122f28042d373186a992b07198094b77238b183e
Talha Imran <talhaimran284@gmail.com>
2020-12-27 19:37:30 +0500

    Protected auth route using JWT token

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({
+      msg: 'No token found!',
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.jwtToken);
+    req.user = decoded.user;
+    console.log(req.user);
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      msg: 'Unauthorized Access, Token is invalid!',
+    });
+  }
+};
--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const auth = require('../../middlewares/auth');
 const router = express.Router();
 
 // @route   GET api/auth
 // @desc    This is a auth route
 // @access  public
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
   res.send('Auth Route!');
 });
 
