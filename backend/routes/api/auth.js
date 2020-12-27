0247da87d37c1a48a06bdb6726d45a3acd1a5588
Talha Imran <talhaimran284@gmail.com>
2020-12-27 20:05:53 +0500

    Protected auth route using JWT token and getting the user data -password when you visit GET /api/auth if the token is valid

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../../models/User');
 const auth = require('../../middlewares/auth');
 const router = express.Router();
 
@@ -6,8 +7,14 @@ const router = express.Router();
 // @desc    This is a auth route
 // @access  public
 
-router.get('/', auth, (req, res) => {
-  res.send('Auth Route!');
+// auth is the middleware here
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).send('Error connecting to the database!');
+  }
 });
 
 module.exports = router;
