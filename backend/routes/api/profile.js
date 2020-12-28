e7505395235a7d9947a231ce8d7f4c14eab35f7c
Talha Imran <talhaimran284@gmail.com>
2020-12-28 18:25:58 +0500

    Created a auth route that return logged in user's profile

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const auth = require('../../middlewares/auth');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+// @route   GET api/profile/me
+// @desc    This route will return the current user's
+//          profile based on the valid token
+// @access  private
 
-// @route   GET api/profile
-// @desc    This is a users route
-// @access  public
+router.get('/me', auth, async (req, res) => {
+  console.log(req.user.id);
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
 
-router.get('/', (req, res) => {
-  res.send('Profile route!');
+    if (!profile) {
+      return res.status(400).send('No profile found!');
+    }
+
+    return res.status(200).json(profile);
+  } catch (error) {
+    return res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
