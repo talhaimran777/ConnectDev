73bdaccfd35001420c5260a00366df548f9c637f
Talha Imran <talhaimran284@gmail.com>
2020-12-29 12:42:04 +0500

    Getting a user's profile based on the user_id

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -115,4 +115,27 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    This route will return a specific user's
+//          profile
+// @access  public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).send('Profile not found');
+    }
+    return res.status(200).json(profile);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(400).send('Profile not found');
+    }
+    return res.status(500).send(err);
+  }
+});
+
 module.exports = router;
